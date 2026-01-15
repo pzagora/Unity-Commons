9e6c9103e63a2bc8d4764433e9268fca1e1b8c55
Piotr Zagóra <piotr@zagora.dev>
2026-01-15 23:05:07 +0100

    Updated Safe helper

--- Runtime/Helpers/Safe.cs
+++ Runtime/Helpers/Safe.cs
@@ -6,20 +6,27 @@ namespace Unity.Commons
     {
         public static void Call(Func<bool> condition, Action action)
         {
-            if (condition()) action();
+            if (condition()) 
+                action();
         }
-        
+
         public static T Get<T>(Func<bool> condition, Func<T> getter, T defaultValue = default)
-            => condition() ? getter() : defaultValue;
+        {
+            return condition()
+                ? getter()
+                : defaultValue;
+        }
 
         public static void Set<T>(Func<bool> condition, Action<T> setter, T value)
         {
-            if (condition()) setter(value);
+            if (condition())
+                setter(value);
         }
 
         public static void Set<T1, T2>(Func<bool> condition, Action<T1, T2> setter, T1 arg1, T2 arg2)
         {
-            if (condition()) setter(arg1, arg2);
+            if (condition()) 
+                setter(arg1, arg2);
         }
     }
 }

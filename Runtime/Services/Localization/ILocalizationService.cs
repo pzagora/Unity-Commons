5b2e6a45ff019bec1e8ac307155cc227113a4b69
Piotr Zagóra <piotr@zagora.dev>
2026-01-31 00:36:52 +0100

    Made ILocalizationService derive from IService

--- Runtime/Services/Localization/ILocalizationService.cs
+++ Runtime/Services/Localization/ILocalizationService.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace Commons.Services
 {
-    public interface ILocalizationService<TLanguage>
+    public interface ILocalizationService<TLanguage> : IService
         where TLanguage : Enum
     {
         /// <summary>
